0571c6d8187ed85abf9e6c839d1595e5378787a1
Steven Webster <swebsterdev@gmail.com>
2025-01-26 00:04:32 +0800

    feat: add mobile and desktop hero images

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
 import styles from '@/app/ui/home.module.css';
 
@@ -34,7 +35,24 @@ export default function Page() {
         </div>
 
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            // width and height are _not_ the image rendering size. They are the dimensions of the
+            // actual image (sometimes) or some other values that represent the aspect ratio of the
+            // raw image.
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+
+          <Image
+            src="/hero-mobile.png"
+            width={520}
+            height={620}
+            className="md:hidden"
+            alt="Screenshot of the dashboard project showing mobile version"
+          />
         </div>
       </div>
     </main>
