87d60c0a1551de028f8c5f909cbca77f751f333f
Steven Webster <swebsterdev@gmail.com>
2025-01-26 22:11:15 +0800

    feat: manage search query in URL search params

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,17 +1,38 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+
+const queryParam = 'query';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
+
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(event): void => {
+          const mutableSearchParams = new URLSearchParams(searchParams);
+
+          event.target.value
+            ? mutableSearchParams.set(queryParam, event.target.value)
+            : mutableSearchParams.delete(queryParam);
+
+          return router.replace(
+            `${pathname}?${mutableSearchParams.toString()}`
+          );
+        }}
+        defaultValue={searchParams.get(queryParam)?.toString()}
       />
+
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
   );
