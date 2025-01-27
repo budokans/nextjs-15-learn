a1b341b2864250e185323329881ddf08665d409c
Steven Webster <swebsterdev@gmail.com>
2025-01-28 00:10:31 +0800

    feat: server action error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -10,20 +10,25 @@ import { sanitiseCreateInvoiceData } from '@/app/lib/utils';
 import { redirect } from 'next/navigation';
 
 export const createInvoice = async (formData: FormData): Promise<void> => {
-  const { customer_id, amount, status, date } = sanitiseCreateInvoiceData(
-    createInvoiceFormDataSchema.parse({
-      customer_id: formData.get('customerId'),
-      amount: formData.get('amount'),
-      status: formData.get('status'),
-    })
-  );
+  try {
+    const { customer_id, amount, status, date } = sanitiseCreateInvoiceData(
+      createInvoiceFormDataSchema.parse({
+        customer_id: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+      })
+    );
 
-  await sql<Invoice>`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customer_id}, ${amount}, ${status}, ${date})
-  `;
+    await sql<Invoice>`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customer_id}, ${amount}, ${status}, ${date})
+    `;
 
-  revalidatePath(afterCreatePath);
+    revalidatePath(afterCreatePath);
+  } catch (error) {
+    console.error(error);
+  }
+
   redirect(afterCreatePath);
 };
 
@@ -33,25 +38,34 @@ export const updateInvoice = async (
   id: string,
   formData: FormData
 ): Promise<void> => {
-  const { customer_id, amount, status } = sanitiseCreateInvoiceData(
-    createInvoiceFormDataSchema.parse({
-      customer_id: formData.get('customerId'),
-      amount: formData.get('amount'),
-      status: formData.get('status'),
-    })
-  );
+  try {
+    const { customer_id, amount, status } = sanitiseCreateInvoiceData(
+      createInvoiceFormDataSchema.parse({
+        customer_id: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+      })
+    );
 
-  await sql<Invoice>`
-    UPDATE invoices
-    SET customer_id = ${customer_id}, amount = ${amount}, status = ${status}
-    WHERE id = ${id}
-  `;
+    await sql<Invoice>`
+      UPDATE invoices
+      SET customer_id = ${customer_id}, amount = ${amount}, status = ${status}
+      WHERE id = ${id}
+    `;
 
-  revalidatePath(afterCreatePath);
+    revalidatePath(afterCreatePath);
+  } catch (error) {
+    console.error(error);
+  }
+
   redirect(afterCreatePath);
 };
 
 export const deleteInvoice = async (id: string): Promise<void> => {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
-  revalidatePath('/dashboard/invoices');
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+  } catch (error) {
+    console.error(error);
+  }
 };
